5c466a5a0ffcfcb5153aa7104a934f86aa074c36
LaironLF <laironlf@gmail.com>
2023-06-08 17:14:19 +0700

    add theathres

--- app/src/main/java/com/example/p070/newtheatre.java
+++ app/src/main/java/com/example/p070/newtheatre.java
@@ -5,20 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class newtheatre extends AppCompatActivity {
     TextView tvInfo;
     EditText tvName;
     EditText tvAddress;
+    ProgressBar progressBar;
     newtheatre.MyTask mt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class newtheatre extends AppCompatActivity {
         tvInfo = (TextView) findViewById(R.id.tvInfo);
         tvName = (EditText) findViewById(R.id.editTextTextPersonName);
         tvAddress = (EditText) findViewById(R.id.editTextTextPersonAddress);
+        progressBar = findViewById(R.id.progress_circular);
+        progressBar.setVisibility(View.INVISIBLE);
     }
     public void onclick(View v) {
         mt = new newtheatre.MyTask();
@@ -37,13 +46,18 @@ public class newtheatre extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             tvInfo.setText("Begin");
+            progressBar.setVisibility(View.VISIBLE);
         }
         @Override
         protected Void doInBackground(String... params) {
-            HttpURLConnection myConnection=null;
+            HttpURLConnection myConnection = null;
+            BufferedOutputStream os = null;
+            //          InputStream is = null;
+            String line = null;
+            String total = "";
+            JSONObject obj = null;
             try {
-                // http://127.0.0.1:8080/newtheatre
-                URL githubEndpoint = new URL("http://10.0.2.2:8080/kino/");
+                URL githubEndpoint = new URL(Data.newTheatre);
                 myConnection =
                         (HttpURLConnection) githubEndpoint.openConnection();
             } catch (MalformedURLException e) {
@@ -51,27 +65,81 @@ public class newtheatre extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            myConnection.setDoOutput(true);
             try {
-                myConnection.getOutputStream().write( ("id=1&Name=" + params[0]+"&address="+params[1]).getBytes());
+                myConnection.setRequestMethod("POST");
+            } catch (ProtocolException e) {
+                throw new RuntimeException(e);
+            }
+            myConnection.setDoInput(true);
+            myConnection.setDoOutput(true);
+            myConnection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            myConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", params[0]);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                jsonObject.put("address", params[1]);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            String message = jsonObject.toString();
+            try {
+                os = new BufferedOutputStream(myConnection.getOutputStream());
             } catch (IOException e) {
-                e.printStackTrace();
-            };
+                throw new RuntimeException(e);
+            }
+            try {
+                os.write(message.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //clean up
+            try {
+                os.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-            int i=0;
+
+            int i = 0;
             try {
                 i = myConnection.getResponseCode();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 //                tvInfo.setText(str);
-            if (i==200) {
+            if (i == 200) {
+                InputStream responseBody = null;
+                try {
+                    responseBody = myConnection.getInputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                BufferedReader r = new BufferedReader(new InputStreamReader(responseBody));
+                while (true) {
+                    try {
+                        if (!((line = r.readLine()) != null)) break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    total = total + line;
+                }
+                try {
+                    obj = new JSONObject(total);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
             return null;
+
         }
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             tvInfo.setText("Добавлено");
+            progressBar.setVisibility(View.INVISIBLE);
         }
     }
 }
