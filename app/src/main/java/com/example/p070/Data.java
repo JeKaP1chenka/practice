5e55a1b6bb9094abe578a1ea0518597aeaddd782
MaximP1henka <87613293+MaximP1henka@users.noreply.github.com>
2023-06-08 16:46:15 +0700

    1

--- app/src/main/java/com/example/p070/Data.java
+++ app/src/main/java/com/example/p070/Data.java
@@ -4,6 +4,10 @@ public class Data {
     static String URL = "http://host1857461.hostland.pro/practice/kino/";
     static String filtrTheatre = URL + "filtrtheatre?name=";
     static String newTheatre = URL + "newtheatre";
+
     static String films = URL + "films?name=";
     static String newFilm = URL + "newfilm";
+
+    static String updateTheatre = URL + "update_theatre";
+    static String deleteTheatre = URL + "delete_theatre";
 }
